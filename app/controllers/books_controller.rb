f31bf364f3de3e40a58ce091a232c1824f614e25
nao0725 <aqua.725.times@gmail.com>
2021-06-14 12:54:55 +0000

    Finish today's coding

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,28 @@
 class BooksController < ApplicationController
 
   def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
   end
 
   def index
+
   end
 
   def show
   end
-  
+
   def edit
   end
-  
+
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :body,:image)
   end
 
 end
