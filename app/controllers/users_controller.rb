ce3fc5b6719b1364dd7ab9e17dc3b994d3fd6d1f
nao0725 <aqua.725.times@gmail.com>
2021-06-16 12:55:49 +0000

    Update edit pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
@@ -21,7 +25,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:profile_image_id, :name)
+    params.permit(:profile_image_id, :name)
   end
 end
 
